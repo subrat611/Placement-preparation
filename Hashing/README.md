0f96eed75f8bc0494d6086edc8a5b96a0c266331
Subrat kumar jena <subratj611@gmail.com>
2023-11-18 18:36:35 +0530

    notes update

---
+++ Hashing/README.md
@@ -0,0 +1,11 @@
+- [What is Hashmap](#what-is-hashmap)
+
+## What is Hashmap?
+
+- Hashmap is a data structure.
+- In hashmap data are stored in key-value pair.
+
+## In-built methods in C++
+
+- map -> O(log n)
+- unordered_map -> O(1)
---
+++ Hashing/map.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+
+    // <key, value>
+    unordered_map<string, int> m;
+
+    // Insertion
+
+    // 1
+    pair<string,int> p = make_pair("s", 10);
+    m.insert(p);
+    
+    // 2
+    pair<string,int> p2("k", 100);
+    m.insert(p2);
+
+    // 3
+    m["a"] = 20;
+    // m["s"] = 100; updation because s key present already
+
+    
+    
+    // Search
+    cout << m["s"] << endl; // 10
+    cout << m.at("k") << endl; // 100
+
+    cout << m.at("unknown") << endl; // out of range key not found
+    cout << m["unknowKey"] << endl; // a new key created if not available and print 0 (because of no value)
+
+    
+
+    // Size
+    cout << m.size() << endl;
+
+
+    /**
+     * To check the presence of key
+     * if key present -> 1
+     * else 0
+    */
+    cout << m.count("s") << endl; // 1
+
+
+
+    // Erase
+    m.erase("k");
+
+
+
+    // Traversing
+    for(auto i: m) {
+        cout << i.first << " " << i.second << endl;
+    }
+    // using iterator 
+    unordered_map<string, int> :: iterator it = m.begin(); // random order
+    // map<string, int> :: iterator it = m.begin(); // order preserved
+
+    while(it != m.end()) {
+        cout << it->first << " " << it->second << endl;
+        it++;
+    }
+
+
+    return 0;
+}
